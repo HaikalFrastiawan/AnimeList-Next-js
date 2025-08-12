06bba196017d01a3ce353957e2daf7fb32b6c22d
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-08-13 05:58:49 +0700

    sesi3

--- src/components/Navbar/InputSearch.js
+++ src/components/Navbar/InputSearch.js
@@ -11,16 +11,25 @@ const InputSearch = () => {
     const router = useRouter()
     
     const handleSearch = (event) =>{
-        event.preventDefault()
-        const keyword = searchRef.current.value
+
+      if( event.key === "Enter" || event.type === "click"){
+         event.preventDefault()
+         const keyword = searchRef.current.value
         router.push(`/search/${keyword}`)
+      } 
+
+       
+    
+       
     }
     return (
+      
         <div className="relative">
               <input 
                 placeholder="Mau anime apa nichh...." 
                 className="bg-white w-full p-2 rounded "
                 ref={searchRef}
+                onKeyDown={handleSearch}
                 />
               <button className="absolute top-2 end-2" onClick={handleSearch}>
                 <MagnifyingGlassIcon size={24} />
